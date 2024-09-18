bc937076a3dc68eeaa3175c5b5758dd4f62ec971
bsatoriu <bsatoriu@jpl.nasa.gov>
2024-09-18 14:21:38 -0700

    syntax bug fix

--- test-secrets.py
+++ test-secrets.py
@@ -7,5 +7,5 @@ maap = MAAP()
 output_dir = sys.argv[1]
 secret_name = sys.argv[2]
 with open(os.path.join(output_dir, "write-dummy-secret.txt"), 'w') as fw:
-    fw.write(secret_name + " value: " maap.secrets.get_secret(secret_name))
+    fw.write(secret_name + " value: " + maap.secrets.get_secret(secret_name))
     fw.write(" Output secret value successfully!")
