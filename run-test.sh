b5604b28de3901f3eb4308a86a43fb9c8c4bd044
bsatoriu <bsatoriu@jpl.nasa.gov>
2024-09-18 13:32:41 -0700

    test-secrets

--- run-test.sh
+++ run-test.sh
@@ -5,6 +5,9 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P)
 OUTPUTDIR="${PWD}/output"
 INPUT_FILE=$(ls -d input/*)
 
+# PARAMS
+SECRET_NAME=$1
+
 mkdir -p ${OUTPUTDIR}
 echo "Testing writing output product"
 python ${basedir}/test-output-product.py ${OUTPUTDIR}
@@ -12,3 +15,7 @@ python ${basedir}/test-output-product.py ${OUTPUTDIR}
 
 echo "Testing opening input file"
 python ${basedir}/test-input-file.py ${INPUT_FILE}
+
+
+echo "Testing getting dummy secret"
+python ${basedir}/test-secret.py ${OUTPUTDIR} ${SECRET_NAME}
---
+++ test-secrets.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import sys
+import os
+from maap.maap import MAAP
+maap = MAAP()
+
+output_dir = sys.argv[1]
+secret_name = sys.argv[2]
+with open(os.path.join(output_dir, "write-dummy-secret.txt"), 'w') as fw:
+    fw.write(secret_name + " value: " maap.secrets.get_secret(secret_name))
+    fw.write(" Output secret value successfully!")
